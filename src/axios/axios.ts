6f66d0dcf8c7ce5952c8a276197373159fb9f609
Boris Ushakov <inlvert@gmail.com>
2025-10-03 13:18:10 +0300

    add new url

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://nextjs-auth-fullstack-dmlw.vercel.app/",
 });
 
 
